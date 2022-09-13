4b1bff988a60c304fe52c298c52d7de06163225b
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-13 11:30:22 +0700

    otw layouting

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,63 +1,57 @@
 <!DOCTYPE html>
-<!--
-Template Name: NobleUI - Laravel Admin Dashboard Template
-Author: NobleUI
-Website: https://www.nobleui.com
-Portfolio: https://themeforest.net/user/nobleui/portfolio
-Contact: nobleui123@gmail.com
-Purchase: https://1.envato.market/nobleui_laravel
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
 <html>
+
 <head>
-  <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Responsive Laravel Admin Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="NobleUI">
-	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, laravel, theme, front-end, ui kit, web">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Responsive Laravel Admin Dashboard Template based on Bootstrap 5">
+    <meta name="author" content="NobleUI">
+    <meta name="keywords"
+        content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, laravel, theme, front-end, ui kit, web">
 
-  <title>NobleUI - Laravel Admin Dashboard Template</title>
+    <title>BAMBU APUS</title>
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-  <!-- End fonts -->
-  
-  <!-- CSRF Token -->
-  <meta name="_token" content="{{ csrf_token() }}">
-  
-  <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
 
-  <!-- plugin css -->
-  <link href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}" rel="stylesheet" />
-  <link href="{{ asset('assets/plugins/flag-icon-css/css/flag-icon.min.css') }}" rel="stylesheet" />
-  <link href="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" />
-  <!-- end plugin css -->
+    <!-- CSRF Token -->
+    <meta name="_token" content="{{ csrf_token() }}">
 
-  @stack('plugin-styles')
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
 
-  <!-- common css -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-  <!-- end common css -->
+    <!-- plugin css -->
+    <link href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/flag-icon-css/css/flag-icon.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" />
+    <!-- end plugin css -->
 
-  @stack('style')
+    @stack('plugin-styles')
+
+    <!-- common css -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <!-- end common css -->
+
+    @stack('style')
 </head>
-<body data-base-url="{{url('/')}}">
 
-  <script src="{{ asset('assets/js/spinner.js') }}"></script>
+<body data-base-url="{{ url('/') }}">
 
-  <div class="main-wrapper" id="app">
-    @include('layout.sidebar')
-    <div class="page-wrapper">
-      @include('layout.header')
-      <div class="page-content">
-        @yield('content')
-      </div>
-      @include('layout.footer')
+    <script src="{{ asset('assets/js/spinner.js') }}"></script>
+
+    <div class="main-wrapper" id="app">
+        @include('layout.sidebar')
+        <div class="page-wrapper">
+            @include('layout.header')
+            <div class="page-content">
+                @yield('content')
+            </div>
+            @include('layout.footer')
+        </div>
     </div>
-  </div>
 
     <!-- base js -->
     <script src="{{ asset('js/app.js') }}"></script>
@@ -75,4 +69,5 @@ License: For each use you must have a valid license purchased only from above li
 
     @stack('custom-scripts')
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+
+});
 
 require __DIR__.'/auth.php';
