7acf46a3dc91a452425a5d444f1471354370b927
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-15 09:21:52 +0700

    fixing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(125);
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         Paginator::useBootstrapFive();
