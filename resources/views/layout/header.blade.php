381c1f505e64d31bcb73102255544f08f1360dd7
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-09-20 11:06:51 +0700

    header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -19,7 +19,7 @@
                         <a href="javascript:;" class="text-muted">Clear all</a> --}}
                     </div>
                     <div class="p-1">
-                        @foreach ($notifications as $notification)
+                        @foreach ($notifications->unreadNotifications as $notification)
                             <a href="{{ $notification->data['url'] }}"
                                 class="dropdown-item d-flex align-items-center py-2">
                                 <div
